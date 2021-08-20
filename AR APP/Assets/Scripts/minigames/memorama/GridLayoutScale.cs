4a7a848e39c611cda68014e7b4dab33093cba630
Alex <1080810@alumno.um.edu.mx>
2021-08-20 08:48:58 -0500

    Add Scalable grid layout to memory game

---
+++ AR APP/Assets/Scripts/minigames/memorama/GridLayoutScale.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridLayoutScale : MonoBehaviour
+{
+	GridLayoutGroup gridLayoutGroup;
+	RectTransform rect;
+	public int cellCount = 8;
+
+	void Start()
+	{
+		gridLayoutGroup = GetComponent<GridLayoutGroup>();
+		rect = GetComponent<RectTransform>();
+
+		gridLayoutGroup.cellSize = new Vector2(rect.rect.height /1.9f, rect.rect.height/1.9f);
+	}
+
+	void OnRectTransformDimensionsChange()
+	{
+		if (gridLayoutGroup != null && rect != null)
+			if ((rect.rect.height + (gridLayoutGroup.padding.horizontal * 2)) * cellCount < rect.rect.width)
+				gridLayoutGroup.cellSize = new Vector2(rect.rect.height / 1.9f, rect.rect.height / 1.9f);
+	}
+}
