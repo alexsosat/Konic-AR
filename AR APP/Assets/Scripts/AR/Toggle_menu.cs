59a316a1593632b4e6b290a18e9ffa49d183440a
alexsosat <1080810@alumno.um.edu.mx>
2022-02-28 20:10:15 -0600

    Added teacher recomendations

---
+++ AR APP/Assets/Scripts/AR/Toggle_menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggle_menu : MonoBehaviour
+{
+    public GameObject menuObject;
+    private bool _isOpen = true;
+    
+    public void ToggleMenu()
+    {
+        menuObject.SetActive(!_isOpen);
+        _isOpen = !_isOpen;
+    }
+}
